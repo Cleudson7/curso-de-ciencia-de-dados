671ad5a1e1ff740a4a5ded64e335d2c9a601b6cc
Cleudson Miranda <cleudson.miranda@gmail.com>
2024-12-04 16:50:09 -0400

    aula 02

--- teste.R
+++ teste.R
@@ -1,2 +1,22 @@
 #testando a sincronia com github
 2+2=
+# Instalando pacotes
+  install.packages("tidyverse")
+
+#carregando os pacotes
+library(tidyverse)
+
+#verificar e se não estiver instalando os pacotes faz a instalação
+
+if(!require(tidyverse)) installed.packages("tidyverse")
+
+# Verificando os pacotes instalados
+
+sessionInfo()
+
+#exemplo de função no R
+x <- c(1,1,2,3,5,8)
+
+# calcula a média de x
+mean(x)
+
